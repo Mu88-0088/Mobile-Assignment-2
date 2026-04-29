20fbaab0ffbbd1b5075d8598e5486189f32ca2ee
Mu88-0088 <musabashik@gmail.com>
2026-04-29 10:25:38 +0300

    Convert CountryExplorerApp to StatefulWidget
    Refactor CountryExplorerApp to use StatefulWidget for theme management.

--- main.dart
+++ main.dart
@@ -5,8 +5,25 @@ void main() {
   runApp(const CountryExplorerApp());
 }
 
-class CountryExplorerApp extends StatelessWidget {
+class CountryExplorerApp extends StatefulWidget {
   const CountryExplorerApp({super.key});
+
+  static _CountryExplorerAppState? of(BuildContext context) =>
+      context.findAncestorStateOfType<_CountryExplorerAppState>();
+
+  @override
+  State<CountryExplorerApp> createState() => _CountryExplorerAppState();
+}
+
+class _CountryExplorerAppState extends State<CountryExplorerApp> {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void toggleTheme() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +34,14 @@ class CountryExplorerApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: _themeMode,
       home: const HomeScreen(),
     );
   }
